0cc0d60132390209e9d9c3c523db2bed32e73c44
Albert G <albert.grekowski@gmail.com>
2023-09-04 16:06:29 +0200

    remove prints

--- MatomoGenerator/src/main/kotlin/com/codingchallenge/matomogenerator/service/ValidatorService.kt
+++ MatomoGenerator/src/main/kotlin/com/codingchallenge/matomogenerator/service/ValidatorService.kt
@@ -8,12 +8,10 @@ import org.springframework.stereotype.Service
 class ValidatorService (val namespaceRepository: NamespaceRepository, val nameEntryRepository: NameEntryRepository) {
 
     fun namespaceExists(nameToValidate:String): Boolean {
-        println("validating namespace")
         return namespaceRepository.findNameSpaceByNamespaceName(nameToValidate) != null
     }
 
     fun nameExistsInNamespace(nameToValidate: String, namespaceToValidate: String): Boolean {
-        println("validating nameEntry")
         val nameSpace = namespaceRepository.findNameSpaceByNamespaceName(namespaceToValidate)
         return nameSpace?.let { nameEntryRepository.findNameEntryByEntryNameAndNamespace(nameToValidate, it) } != null
     }
